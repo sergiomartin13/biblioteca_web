0573989849b6af1caaa58f7708713325efbef262
ahergar2009 <ahergar2009@seritium.es>
2026-03-22 13:30:22 +0100

    añadidos comentarios al código

--- tests/test_adaptadores.py
+++ tests/test_adaptadores.py
@@ -3,39 +3,39 @@ import tempfile
 from adaptadores.repositorio_fichero import RepositorioFichero
 from dominio.publicacion import Libro, Revista
 
-
+#clase test repositorio
 class TestRepositorioFichero:
-    def setup_method(self):
+    def setup_method(self):#crear entorno
         self.temp_file = tempfile.NamedTemporaryFile(
             delete=False, mode='w', suffix='.txt'
         )
         self.temp_file.close()
         self.repositorio = RepositorioFichero(self.temp_file.name)
 
-    def teardown_method(self):
+    def teardown_method(self):#limpiar archivos
         os.unlink(self.temp_file.name)
 
-    def test_guardar_libro(self):
+    def test_guardar_libro(self):#guarda libro
         libro = Libro("Libro Test")
         self.repositorio.guardar(libro)
         with open(self.temp_file.name, 'r') as f:
             content = f.read()
             assert "Libro;Libro Test;False\n" == content
 
-    def test_guardar_revista(self):
+    def test_guardar_revista(self):#guarda revista
         revista = Revista("Revista Test")
         self.repositorio.guardar(revista)
         with open(self.temp_file.name, 'r') as f:
             content = f.read()
             assert "Revista;Revista Test;False\n" == content
 
-    def test_cargar_todas_vacio(self):
+    def test_cargar_todas_vacio(self):#cargar archivos sin datos
         publicaciones = self.repositorio.cargar_todas()
         assert publicaciones == []
 
-    def test_cargar_todas_con_datos(self):
+    def test_cargar_todas_con_datos(self):#cargar archivos con datos
         with open(self.temp_file.name, 'w') as f:
-            f.write("Libro;Libro 1;True\n")
+            f.write("Libro 1;Libro 1;True\n")#("Libro 1;Libro 1;True\n")
             f.write("Revista;Revista 1;False\n")
         publicaciones = self.repositorio.cargar_todas()
         assert len(publicaciones) == 2
@@ -46,16 +46,16 @@ class TestRepositorioFichero:
         assert publicaciones[1].prestado is False
         assert isinstance(publicaciones[1], Revista)
 
-    def test_borrar_existente(self):
+    def test_borrar_existente(self):#borrar archivos existentes
         with open(self.temp_file.name, 'w') as f:
             f.write("Libro;Libro 1;False\n")
             f.write("Revista;Revista 1;False\n")
-        self.repositorio.borrar("Libro 1")
+        self.repositorio.borrar("Libro 1")#Borrar Revista 1 también
         with open(self.temp_file.name, 'r') as f:
             content = f.read()
             assert content == "Revista;Revista 1;False\n"
 
-    def test_borrar_no_existente(self):
+    def test_borrar_no_existente(self):#borrar archivos no existentes
         with open(self.temp_file.name, 'w') as f:
             f.write("Libro;Libro 1;False\n")
         self.repositorio.borrar("No existe")
